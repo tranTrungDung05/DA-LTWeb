dffafbb25fdf5603de502213721c06235d3aa579
ImanuBae <imanutttt@gmail.com>
2026-06-18 21:07:01 +0700

    feat: add invoice publishing schema

---
+++ Migrations/20260618124009_AddInvoicePublishing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace smart_hostel_management_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvoicePublishing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "Invoices",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishedAt",
+                table: "Invoices",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "PublishedAt",
+                table: "Invoices");
+        }
+    }
+}
--- Models/Core/Invoice.cs
+++ Models/Core/Invoice.cs
@@ -13,6 +13,8 @@ namespace smart_hostel_management_system.Models.Core
         public DateTime IssueDate { get; set; } = DateTime.Now;
         public DateTime DueDate { get; set; }
         public bool IsOutDatePay { get; set; }
+        public bool IsPublished { get; set; }
+        public DateTime? PublishedAt { get; set; }
         public InvoiceStatus Status { get; set; } = InvoiceStatus.Unpaid;
 
         public int RoomId { get; set; }
